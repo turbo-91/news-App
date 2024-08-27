982f7106a016b23e5ee6c53e89f8178a2b2f2c79
Torben Jost <torbenjost@proton.me>
2024-08-27 16:43:26 +0200

    set up next auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import GlobalStyle from "../styles";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 }
