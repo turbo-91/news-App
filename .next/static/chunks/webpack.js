68a025bb09c1c3a138993e9e006070f49198d405
Torben Jost <torbenjost@proton.me>
2024-08-27 16:11:42 +0200

    fix spotlight component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3c529c8d8397ba71"; }
+/******/ 		__webpack_require__.h = function() { return "6e3495b78f0aae67"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
