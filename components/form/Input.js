ec7c5751c95937a31b87bb9b5842d59212bbfc03
Torben Jost <torbenjost@proton.me>
2024-08-27 11:54:49 +0200

    create Input component

---
+++ components/form/Input.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Input = styled.input`
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 0.75rem;
+  width: 100%;
+  line-height: 1.5;
+`;
+
+function Input() {
+  return <Input />;
+}
+
+export default Input;
