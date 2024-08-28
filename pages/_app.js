e3d795b5216992803c2c15680140318398979181
Torben Jost <torbenjost@proton.me>
2024-08-28 15:40:11 +0200

    add function for favorite toggling

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from "@/layout/Layout";
 import GlobalStyle from "../styles";
 import { SessionProvider } from "next-auth/react";
 import useLocalStorageState from "use-local-storage-state";
+import toggleFavorite from "@/utils/handleToggleFavorite";
 
 export default function App({
   Component,
@@ -15,12 +16,21 @@ export default function App({
     }
   );
 
+  function handleToggleFavorite(url, userId) {
+    const updatedArticles = toggleFavorite(favoriteArticles, url, userId);
+    setFavoriteArticles(updatedArticles);
+  }
+
   return (
     <>
       <GlobalStyle />
       <SessionProvider session={session}>
         <Layout>
-          <Component {...pageProps} favoriteArticles={favoriteArticles} />
+          <Component
+            {...pageProps}
+            favoriteArticles={favoriteArticles}
+            handleToggleFavorite={handleToggleFavorite}
+          />
         </Layout>
       </SessionProvider>
     </>
---
+++ utils/handleToggleFavorite.js
@@ -0,0 +1,18 @@
+export default function toggleFavorite(favoriteArticles, url, userId) {
+  // See if article is already in the state array
+  const info = favoriteArticles.find(
+    (article) => article.url === url && article.userId === userId
+  );
+
+  if (info) {
+    // If the article is already in the array, toggle the isFavorite value
+    return favoriteArticles.map((article) =>
+      article.url === url && article.userId === userId
+        ? { ...article, isFavorite: !article.isFavorite }
+        : article
+    );
+  } else {
+    // If the article is not in the array already, add it with the favorite as true
+    return [...favoriteArticles, { url, userId, isFavorite: true }];
+  }
+}
