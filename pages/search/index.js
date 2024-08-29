f139f90c7248793b63acbce76da8175e02a19691
Torben Jost <torbenjost@proton.me>
2024-08-29 13:01:18 +0200

    fix styling problem pagination

--- pages/search/index.js
+++ pages/search/index.js
@@ -14,7 +14,7 @@ const PaginationContainer = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 20px;
-  margin-bottom: 2rem;
+  margin-bottom: 10vh;
 `;
 
 const PaginationButton = styled.button`
